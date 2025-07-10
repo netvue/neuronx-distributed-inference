2806e042b511c3cfbc7797ad8e7714732b0e5117
Panqing Huang <panqing.huang@netvue.com>
2025-07-10 11:45:40 +0800

    fix

--- src/neuronx_distributed_inference/models/florence2/modeling_florence2.py
+++ src/neuronx_distributed_inference/models/florence2/modeling_florence2.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import math
-from typing import Optional, Tuple
+from typing import Optional, Tuple, List
 from collections import OrderedDict
 
 import torch
