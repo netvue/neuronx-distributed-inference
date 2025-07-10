aaf54d8a80c8ccb69d4ddd5e65fddf4c75f56e3b
Panqing Huang <panqing.huang@netvue.com>
2025-07-10 15:10:37 +0800

    fix

--- src/neuronx_distributed_inference/models/florence2/modeling_florence2.py
+++ src/neuronx_distributed_inference/models/florence2/modeling_florence2.py
@@ -524,6 +524,7 @@ class NeuronFlorence2Attention(NeuronAttentionBase):
         self.neuron_config = config.neuron_config
         self.hidden_size = config.hidden_size
         self.num_attention_heads = config.num_attention_heads
+        self.num_attention_heads_per_partition = self.num_attention_heads
         self.num_key_value_heads = config.num_key_value_heads
         self.head_dim = self.hidden_size // self.num_attention_heads
         self.tp_degree = config.neuron_config.tp_degree
